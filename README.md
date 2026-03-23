77c228796a0743824d2763cc06f6c785acaee0b0
kpomer <pomerkevin@gmail.com>
2026-03-23 20:28:00 +0200

    added github actions workflow and included print lines for logs

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [x] Call brewery DB metadata API to gather full count of breweries
   - https://api.openbrewerydb.org/v1/breweries/meta
 
-- [x] Use rundate [-1, 0, +1] as seed for random number between 1 and total breweries
+- [x] Use rundate [-2 to +10] as seed for random number between 1 and total breweries
 
 - [x] Call the brewery DB once with each seed_value
   - https://api.openbrewerydb.org/v1/breweries?page={seed_value}&per_page=1
@@ -16,7 +16,7 @@
 
 - [ ] Add more robust handling for http requests. These should use a try/catch to handle possible timeouts or other errors. If something is failing, notify myself via email or ntfy notification to fix the data. Note - Gist can be manually updated if necessary
 
-- [ ] Setup cron job as Github actions to refresh this data every 24hrs
+- [ ] Setup cron job as Github actions to refresh this data every X days (likely once per week)
 
 - [ ] Setup TRMNL plug-in to retrieve this data from the Gist based on the current day
 
--- src/main.py
+++ src/main.py
@@ -7,14 +7,15 @@ import gist_client
 
 def main(dateString):
     
-    inputDate = datetime.datetime.strptime(dateString, "%Y-%m-%d") # Primary Date for Data Retrieval
+    inputDate = datetime.datetime.strptime(dateString, '%Y-%m-%d') # Reference Date for Data Retrieval
     breweryTotalCount = api_client.getBreweryCount() # Total count of breweries in DB
+    print(f"Updating Breweries based on reference date: {dateString}")
 
     breweries_dict = {}
     # Retrieve brewery data for dates around inputDate (including several days before/after to avoid daily updates and handle all timezones)
     for offset in range(-2, 10):
         checkDate = inputDate + datetime.timedelta(days=offset)
-        checkDateString = checkDate.strftime("%Y-%m-%d")
+        checkDateString = checkDate.strftime('%Y-%m-%d')
 
         random.seed(checkDateString) # "random" breweryIndex is deterministic based on checkDateString seed value
         breweryIndex = random.randint(1, breweryTotalCount)
@@ -24,15 +25,16 @@ def main(dateString):
     breweriesOfTheDay = json.dumps(breweries_dict, indent=2)
 
     # Publish Data to Github Gist
-    gistDescription = f"Brewery Data - Updated {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}"
+    gistDescription = f"Brewery Data - Updated {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
     gist_client.updateGistData(gistDescription, breweriesOfTheDay)
+    print(f"Brewery Data has been updated with {len(breweries_dict)} entries")
 
 
 if __name__ == "__main__":
     date_input = ""
     if len(sys.argv) == 1:
         # No date argument - use current date
-        date_input = datetime.datetime.now().strftime("%Y-%m-%d")
+        date_input = datetime.datetime.now().strftime('%Y-%m-%d')
     elif len(sys.argv) == 2:
         # Use date provided as argument
         date_input = sys.argv[1]
